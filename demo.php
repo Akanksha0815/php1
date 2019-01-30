d9b23b192d91c45f1091d661d77a0145757fcfe1
Akanksha0815 <prachi0615@gmail.com>
2019-01-30 15:35:37 +0530

    all files upload

---
+++ demo.php
@@ -0,0 +1,22 @@
+<?php
+
+$a=10;
+$b=20;
+$c=30;
+$d=10;
+
+if($a==$b)
+{
+	echo "a is equal to b";
+}
+elseif($a==$c)
+{
+	echo "a is equal to c";
+}
+else if($a==$d)
+{
+	echo "a is equal to d";
+}
+
+
+?>
